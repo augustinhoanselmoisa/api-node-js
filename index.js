04b1cf1d4593ee508a44a5da56b0cfc8e8f90875
augustinhoanselmoisa <isabelaaugustinho5@gmail.com>
2025-04-07 08:23:58 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-const express =require('express');
+require('dotenv').config();
+const express = require('express'); 
+const cors = require('cors');
 
-import express from'express';
 
-const app =express();
+const app = express(); 
+app.use(cors()); 
+app.use(express.json()); 
 
-const porta = 3333;
-app.listen(porta,()=>{
-    console.log(`servidor iniciado na porta ${porta}`);
+const porta = process.env.PORT || 3333;
+
+app.listen(porta, () => {
+    console.log(`Servidor iniciado em http://localhost:${porta}`);
 });
+
+app.get('/', (request, response) => {
+    response.send('Hello World');
+});
+
