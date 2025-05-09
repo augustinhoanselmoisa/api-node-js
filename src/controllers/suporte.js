3405651748ed56ac47e32b73d565a724e776a02e
augustinhoanselmoisa <isabelaaugustinho5@gmail.com>
2025-05-09 10:30:06 -0300

    insert

--- src/controllers/suporte.js
+++ src/controllers/suporte.js
@@ -30,10 +30,41 @@ module.exports = {
     }, 
     async cadastrarSuporte(request, response) {
         try {
+
+
+            const { usu_id, asst_id, sup_mensagem, sup_status, sup_data_criacao, sup_email, sup_nome  } = request.body;
+const usu_ativo = 1;
+
+// instrução SQL
+const sql = `
+    INSERT INTO suporte
+        (usu_id, asst_id, sup_mensagem, sup_status, sup_data_criacao, sup_email, sup_nome )
+    VALUES
+        (?, ?, ?, ?, ?, ?, ?);
+`;
+
+// definição dos dados a serem inseridos em um array
+const values = [ usu_id, asst_id, sup_mensagem, sup_status, sup_data_criacao, sup_email, sup_nome ];
+
+// execução da instrução sql passando os parâmetros
+const [result] = await db.query(sql, values);
+
+// identificação do ID do registro inserido
+const dados = {
+    id: result.insertId,
+    usu_id,
+    asst_id, 
+    sup_mensagem, 
+    sup_status,
+    sup_data_criacao, 
+    sup_email,
+    sup_nome
+
+};
             return response.status(200).json({
                 sucesso: true, 
                 mensagem: 'Cadastro de Suporte', 
-                dados: null
+                dados: dados
             });
         } catch (error) {
             return response.status(500).json({
